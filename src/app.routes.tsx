16a5fe71a55a27c3d7d5efc1d745d9ba1379337c
Nadzeya Kartashevich <nadovkanady@gmail.com>
2024-01-25 21:10:55 +0300

    fix bag

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,20 +1,18 @@
-import SuspenseComponent from 'components/suspense';
-import React, { FC, Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-
+import SuspenseComponent from "components/suspense";
+import React, { FC, Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const PrivateRoute: FC<{ element: any }> = ({ element: Element }) => {
-  return localStorage.getItem('access_token') ? (
+  return sessionStorage.getItem("accessToken") ? (
     <Suspense fallback={<SuspenseComponent />}>
       <div>
         <Element />
       </div>
     </Suspense>
   ) : (
-    <Navigate to={''} />
+    <Navigate to={""} />
   );
 };
-
 
 const PublicRoute: FC<{ element: any }> = ({ element: Element }) => (
   <Suspense fallback={<SuspenseComponent />}>
@@ -22,40 +20,28 @@ const PublicRoute: FC<{ element: any }> = ({ element: Element }) => (
   </Suspense>
 );
 
-
-const FlightsPage = React.lazy(() => import('app/flights'));
-const SignPage = React.lazy(() => import('./Airways_Common/components/auth'));
-const AdminsPage = React.lazy(() => import('app/admin'));
-const ChatPage = React.lazy(() => import('app/chat'));
-const ManagersPage = React.lazy(() => import('app/manager'));
-
+const FlightsPage = React.lazy(() => import("app/flights"));
+const SignPage = React.lazy(() => import("./Airways_Common/components/auth"));
+const AdminsPage = React.lazy(() => import("app/admin"));
+const ChatPage = React.lazy(() => import("app/chat"));
+const ManagersPage = React.lazy(() => import("app/manager"));
 
 const AppRoutes = () => {
   return (
     <Routes>
-      
-
-
       {/* PUBLIC */}
-      <Route path={'/flights/*'} element={<PrivateRoute element={FlightsPage} />} />
       <Route
-        path={'/auth/*'}
-        element={<PublicRoute element={SignPage} />}
+        path={"/flights/*"}
+        element={<PrivateRoute element={FlightsPage} />}
       />
-      <Route
-        path={'/admin/*'}
-        element={<PublicRoute element={AdminsPage} />}
-      />
+      <Route path={"/auth/*"} element={<PublicRoute element={SignPage} />} />
+      <Route path={"/admin/*"} element={<PublicRoute element={AdminsPage} />} />
       {/* PRIVATE */}
-      <Route path={'/chats/*'} element={<PrivateRoute element={ChatPage} />} />
-      <Route path={'/manager/*'} element={<PrivateRoute element={ManagersPage} />} />
-
-     
-       
-    
-
-    
-    
+      <Route path={"/chats/*"} element={<PrivateRoute element={ChatPage} />} />
+      <Route
+        path={"/manager/*"}
+        element={<PrivateRoute element={ManagersPage} />}
+      />
 
       {/* DEFAULT */}
 
